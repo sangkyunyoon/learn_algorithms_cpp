76e75248a8275d94dfd3c45fcc49f9348c2a20d8
Sangkyun Yoon <sangkyun.yoon@gmail.com>
2021-03-12 14:17:15 +0900

    선택정렬 추가 작업

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ Learn Algorithms using C++
 
 알고리즘 공부한 내용을 정리하고 공유합니다.
 
-# [1. 이진 트리의 구현과 순회](binarytree/README.md)
+## 알고리즘의 시간 복잡도 개념
+
+
+## [1. 이진 트리의 구현과 순회](binarytree/README.md)
+
+## [2. 선택정렬](selection_sort/README.md)
+일반적으로 알고리즘을 공부할 때 가장 먼저 풀어보는 문제는 '정렬(Sort)' 문제입니다. 
+왜냐하면 정렬만큼 알고리즘의 효율성 차이를 극명하게 보여주는 것이 없기 때문입니다. 
---
+++ selection_sort/main.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Sangkyun Yoon on 3/12/21.
+//
+#include <iostream>
+
+int main() {
+    int min, index, temp;
+    int arr[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+    //정렬을 해야하는 수 만큼 루프를 돈다.
+    for (int i = 0; i < 10; ++i) {
+        //항상 최소값은 가장 큰수보다 큰 값으로 매번 초기화 되어야 한다.
+        min = 9999;
+        //정렬 대상을 최소값과 하나씩 비교하면서 정렬되지 않은 대상에서 최소값을 찾는다. 값과 위치 정보 획득
+        for (int j = i; j < 10; ++j) {
+            if(min > arr[j]) {
+                min = arr[j];
+                index = j;
+            }
+        }
+        //i번째 위치의 값과 최소값을 스왑한다.
+        temp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = temp;
+    }
+
+    //배열의 수 만큼 루프를 돌면서 값을 출력한다.
+    for (int k = 0; k < 10; ++k) {
+        std::cout << arr[k] << " ";
+    }
+
+    return 0;
+}
