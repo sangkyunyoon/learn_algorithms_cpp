25212e52629bc18e96a60362ec3f1a5d4c6b4dfa
Sangkyun Yoon <sangkyun.yoon@gmail.com>
2021-03-16 18:41:51 +0900

    퀵정렬 추가

--- quick_sort/main.cpp
+++ quick_sort/main.cpp
@@ -3,7 +3,48 @@
 //
 #include <iostream>
 using namespace std;
+
+void quick_sort(int *data, int start, int end) {
+    if(start >= end) { //원소가 1개인 경우
+        return;
+    }
+
+    int key = start;
+    int i = start + 1;
+    int j = end;
+    int temp;
+
+    while(i <= j) { //엇갈릴 때까지 반복
+        while(data[i] <= data[key]) { //키 값보다 큰 값을 만날 때
+            i++;
+        }
+        while(data[j] >= data[key] && j > start) {
+            j--;
+        }
+        // {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+        if(i > j) {
+            temp = data[j];
+            data[j] = data[key];
+            data[key] = temp;
+        } else {
+            temp = data[j];
+            data[j] = data[i];
+            data[i] = temp;
+        }
+    }
+
+    quick_sort(data, start, j - 1);
+    quick_sort(data, j + 1, end);
+
+}
 int main() {
-    cout << "Hello World";
+    int number = 10;
+    int data[] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+    quick_sort(data, 0, number - 1);
+
+    for (int i = 0; i < number; ++i) {
+        cout << data[i] << " ";
+    }
     return 0;
 }
