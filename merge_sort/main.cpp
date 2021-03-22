c680d76158baa9ef09ab51cb201052b57da26fa2
Sangkyun Yoon <sangkyun.yoon@gmail.com>
2021-03-23 08:16:20 +0900

    병합정렬 작성중

--- merge_sort/main.cpp
+++ merge_sort/main.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 
+void merge_sort(int *pInt, int pInt1[-1], int i, int i1);
+
 void swap(int& a, int& b) {
     int temp = a;
     a = b;
@@ -13,6 +15,24 @@ void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+void merge(int *data, int *tmp, int start, int mid, int end) {
+    if(start < mid)
+}
+
+void merge_sort(int *data, int *tmp, int start, int end) {
+    if(start < end) {
+        int mid = (start + end) / 2;
+        merge_sort(data, tmp, start, mid); //partition
+        merge_sort(data, tmp, mid + 1, end);//partition
+        merge(data, tmp, start, mid, end); //merge & sort
+    }
+}
+
+void merge_sort(int* data, int length) {
+    int tmp[length];
+    merge_sort(data, tmp, 0, length - 1);
 }
 
 int main() {
