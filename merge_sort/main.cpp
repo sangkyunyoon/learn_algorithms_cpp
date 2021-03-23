1bfafd37f4f80f6fa7fd9f843e77ddce631f6acb
Sangkyun Yoon <sangkyun.yoon@gmail.com>
2021-03-23 10:38:27 +0900

    병합정력 작성 완료

--- merge_sort/main.cpp
+++ merge_sort/main.cpp
@@ -2,23 +2,35 @@
 // Created by Sangkyun Yoon on 3/12/21.
 //
 #include <iostream>
+using namespace std;
 
-void merge_sort(int *pInt, int pInt1[-1], int i, int i1);
-
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void print_array(int *data, int length) {
+    for (int i = 0; i < length; ++i) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
 }
 
 void merge(int *data, int *tmp, int start, int mid, int end) {
-    if(start < mid)
+    for (int i = start; i <= end; i++) {
+        tmp[i] = data[i];
+    }
+    int part1 = start;
+    int part2 = mid + 1;
+    int index = start;
+    while (part1 <= mid && part2 <= end) {
+        if(tmp[part1] <= tmp[part2]) {
+            data[index] = tmp[part1];
+            part1++;
+        } else {
+            data[index] = tmp[part2];
+            part2++;
+        }
+        index++;
+    }
+    for (int i = 0; i <= mid - part1; i++) {
+        data[index + i] = tmp[part1 + i];
+    }
 }
 
 void merge_sort(int *data, int *tmp, int start, int end) {
@@ -30,12 +42,16 @@ void merge_sort(int *data, int *tmp, int start, int end) {
     }
 }
 
-void merge_sort(int* data, int length) {
+void merge_sort(int *data, int length) {
     int tmp[length];
     merge_sort(data, tmp, 0, length - 1);
 }
 
 int main() {
-
+    int length = 10;
+    int data[] = {3, 9, 4, 7, 5, 0, 1, 6, 8, 2};
+    print_array(data, length);
+    merge_sort(data, length);
+    print_array(data, length);
     return 0;
 }
